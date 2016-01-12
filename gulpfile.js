7b8d08bfc949cb4b8db81ed3b1ddbb68a613b302
cellistlondon <cthomas.london@gmail.com>
2016-01-12 23:42:38 +0000

    moved bootstrap build out of main build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var path = require('path');
+var concatCss = require('gulp-concat-css');
+var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
 var cp = require('child_process');
 
@@ -7,6 +9,7 @@ var bootstrap_path = path.join(__dirname, 'node_modules', 'bootstrap');
 
 console.log('cellist-co-site: building bootstrap from: ' + bootstrap_path);
 
+//used to build bootstrap
 var gulp_grunt = require('gulp-grunt')(gulp, {
     base: bootstrap_path,
     prefix: 'gulp-grunt-bootstrap-',
@@ -14,25 +17,27 @@ var gulp_grunt = require('gulp-grunt')(gulp, {
 });
 
 var paths = {
-    bootstrap_css: [bootstrap_path + '/dist/css/*.min.css'],
+    bootstrap_mincss: [bootstrap_path + '/dist/css/*.min.css'],
     bootstrap_fonts: [bootstrap_path + '/dist/fonts/*'],
-    bootstrap_js: [bootstrap_path + '/dist/js/*.min.js'],
+    bootstrap_minjs: [bootstrap_path + '/dist/js/*.min.js'],
     dist_css: './css',
     dist_fonts: './fonts',
     dist_js: './js'
 };
 
-gulp.task('css', ['gulp-grunt-bootstrap-dist'], function() {
-    return gulp.src(paths.bootstrap_css)
+gulp.task('css', function() {
+    return gulp.src(paths.bootstrap_mincss)
+        .pipe(concatCss('cellist.css'))
         .pipe(gulp.dest(paths.dist_css));
 });
 
-gulp.task('js', ['gulp-grunt-bootstrap-dist'], function() {
-    return gulp.src(paths.bootstrap_js)
+gulp.task('js', function() {
+    return gulp.src(paths.bootstrap_minjs)
+        .pipe(concat('cellist.js'))
         .pipe(gulp.dest(paths.dist_js));
 });
 
-gulp.task('fonts', ['gulp-grunt-bootstrap-dist'], function() {
+gulp.task('fonts', function() {
     return gulp.src(paths.bootstrap_fonts)
         .pipe(gulp.dest(paths.dist_fonts));
 });
@@ -45,10 +50,7 @@ gulp.task('jekyll-build', ['assets'], function(done) {
         .on('close', done);
 });
 
-gulp.task('finish',['jekyll-build'], function() {
-    //required to exit time-grunt launched within the bootstrap dist grunt task
-    process.exit();
-});
+
 
 /**
  * Wait for jekyll-build, then launch the Server
@@ -64,3 +66,14 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
 });
 
 gulp.task('default',['browser-sync']);
+
+//not required unless bootstrap source modified (not recommended)
+//install dependent npm packages inside bootstrap directory before running:
+//  cd node_modules/bootstrap
+//  npm install
+gulp.task('bootstrap-build',['finish-bootstrap']);
+
+gulp.task('finish-bootstrap',['gulp-grunt-bootstrap-dist'], function() {
+    //required to exit time-grunt launched within the bootstrap dist grunt task
+    process.exit();
+});
